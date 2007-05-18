49bf34ef5dfcf53e0744e816a396cf97a9cfb530
Sebastian Sauer <mail@dipe.org>
2007-05-18 17:49:33 +0000

    try the with cmake shipped FindJNI.cmake first and if failed try our own...
    svn path=/trunk/playground/bindings/krossjava/; revision=666071

--- java/KrossJNIConfig.cmake
+++ java/KrossJNIConfig.cmake
@@ -8,120 +8,77 @@
 #  JAVA_INCLUDE_PATH2    = the include path to jni.h
 #  JAVA_AWT_INCLUDE_PATH = the include path to jawt.h
 
-#macro_optional_find_package(JNI)
-message(STATUS "using FindKrossJNI.cmake")
+# first we try to use the FindJNI.cmake shipped with cmake
+macro_optional_find_package(JNI)
+if(JAVA_INCLUDE_PATH AND JAVA_JVM_LIBRARY)
+    # seems FindJNI.cmake did detect it successful
+    message(STATUS "using FindJNI.cmake")
+else(JAVA_INCLUDE_PATH AND JAVA_JVM_LIBRARY)
+    # FindJNI.cmake failed to detect our jvm, so we do try some custom stuff now
+    message(STATUS "using FindKrossJNI.cmake")
 
-SET(JAVA_AWT_LIBRARY_DIRECTORIES
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Development Kit\\1.4;JavaHome]/lib"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Development Kit\\1.3;JavaHome]/lib"
-  $ENV{JAVA_HOME}/jre/lib/i386
-  $ENV{JAVA_HOME}/jre/lib/amd64
-  /usr/lib
-  /usr/local/lib
-  /usr/lib/java/jre/lib/i386
-  /usr/local/lib/java/jre/lib/i386
-  /usr/local/share/java/jre/lib/i386
-  /usr/lib/j2sdk1.4-sun/jre/lib/i386
-  /usr/lib/j2sdk1.5-sun/jre/lib/i386
-  /opt/sun-jdk-1.5.0.04/jre/lib/amd64
-  /usr/lib/java/jre/lib/amd64
-  /usr/local/lib/java/jre/lib/amd64
-  /usr/local/share/java/jre/lib/amd64
-  /usr/lib/j2sdk1.4-sun/jre/lib/amd64
-  /usr/lib/j2sdk1.5-sun/jre/lib/amd64
-/usr/lib/gcj-4.1/
-  )
-
-SET(JAVA_JVM_LIBRARY_DIRECTORIES)
-FOREACH(dir ${JAVA_AWT_LIBRARY_DIRECTORIES})
-  SET(JAVA_JVM_LIBRARY_DIRECTORIES
-    ${JAVA_JVM_LIBRARY_DIRECTORIES}
-    "${dir}"
-    "${dir}/client"
-    "${dir}/server"
+    SET(JAVA_AWT_LIBRARY_DIRECTORIES
+        /usr/lib/gcj-4.1/
     )
-ENDFOREACH(dir)
 
+    SET(JAVA_JVM_LIBRARY_DIRECTORIES)
+    FOREACH(dir ${JAVA_AWT_LIBRARY_DIRECTORIES})
+        SET(JAVA_JVM_LIBRARY_DIRECTORIES
+            ${JAVA_JVM_LIBRARY_DIRECTORIES}
+            "${dir}"
+            "${dir}/client"
+            "${dir}/server"
+            )
+    ENDFOREACH(dir)
 
-SET(JAVA_AWT_INCLUDE_DIRECTORIES
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Development Kit\\1.4;JavaHome]/include"
-  "[HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Development Kit\\1.3;JavaHome]/include"
-  $ENV{JAVA_HOME}/include
-  /usr/include 
-  /usr/local/include
-  /usr/lib/java/include
-  /usr/local/lib/java/include
-  /usr/local/share/java/include
-  /usr/lib/j2sdk1.4-sun/include
-  /usr/lib/j2sdk1.5-sun/include
-  /opt/sun-jdk-1.5.0.04/include
-/usr/lib/gcc/i486-linux-gnu/4.1.2/include
-  )
+    SET(JAVA_AWT_INCLUDE_DIRECTORIES
+        /usr/lib/gcc/i486-linux-gnu/4.1.2/include
+    )
 
-FOREACH(JAVA_PROG "${JAVA_RUNTIME}" "${JAVA_COMPILE}" "${JAVA_ARCHIVE}")
-  GET_FILENAME_COMPONENT(jpath "${JAVA_PROG}" PATH)
-  FOREACH(JAVA_INC_PATH ../include ../java/include ../share/java/include)
-    IF(EXISTS ${jpath}/${JAVA_INC_PATH})
-      SET(JAVA_AWT_INCLUDE_DIRECTORIES ${JAVA_AWT_INCLUDE_DIRECTORIES} "${jpath}/${JAVA_INC_PATH}")
-    ENDIF(EXISTS ${jpath}/${JAVA_INC_PATH})
-  ENDFOREACH(JAVA_INC_PATH)
-  FOREACH(JAVA_LIB_PATH 
-    ../lib ../jre/lib ../jre/lib/i386 
-    ../java/lib ../java/jre/lib ../java/jre/lib/i386 
-    ../share/java/lib ../share/java/jre/lib ../share/java/jre/lib/i386)
-    IF(EXISTS ${jpath}/${JAVA_LIB_PATH})
-      SET(JAVA_AWT_LIBRARY_DIRECTORIES ${JAVA_AWT_LIBRARY_DIRECTORIES} "${jpath}/${JAVA_LIB_PATH}")
-    ENDIF(EXISTS ${jpath}/${JAVA_LIB_PATH})
-  ENDFOREACH(JAVA_LIB_PATH)
-ENDFOREACH(JAVA_PROG)
+    FOREACH(JAVA_PROG "${JAVA_RUNTIME}" "${JAVA_COMPILE}" "${JAVA_ARCHIVE}")
+        GET_FILENAME_COMPONENT(jpath "${JAVA_PROG}" PATH)
+        FOREACH(JAVA_INC_PATH ../include ../java/include ../share/java/include)
+            IF(EXISTS ${jpath}/${JAVA_INC_PATH})
+                SET(JAVA_AWT_INCLUDE_DIRECTORIES ${JAVA_AWT_INCLUDE_DIRECTORIES} "${jpath}/${JAVA_INC_PATH}")
+            ENDIF(EXISTS ${jpath}/${JAVA_INC_PATH})
+        ENDFOREACH(JAVA_INC_PATH)
+        FOREACH(JAVA_LIB_PATH 
+            ../lib ../jre/lib ../jre/lib/i386 
+            ../java/lib ../java/jre/lib ../java/jre/lib/i386 
+            ../share/java/lib ../share/java/jre/lib ../share/java/jre/lib/i386)
+            IF(EXISTS ${jpath}/${JAVA_LIB_PATH})
+                SET(JAVA_AWT_LIBRARY_DIRECTORIES ${JAVA_AWT_LIBRARY_DIRECTORIES} "${jpath}/${JAVA_LIB_PATH}")
+            ENDIF(EXISTS ${jpath}/${JAVA_LIB_PATH})
+        ENDFOREACH(JAVA_LIB_PATH)
+    ENDFOREACH(JAVA_PROG)
 
-IF(APPLE)
-  IF(EXISTS ~/Library/Frameworks/JavaVM.framework)
-    SET(JAVA_HAVE_FRAMEWORK 1)
-  ENDIF(EXISTS ~/Library/Frameworks/JavaVM.framework)
-  IF(EXISTS /Library/Frameworks/JavaVM.framework)
-    SET(JAVA_HAVE_FRAMEWORK 1)
-  ENDIF(EXISTS /Library/Frameworks/JavaVM.framework)
-  IF(JAVA_HAVE_FRAMEWORK)
-    IF(NOT JAVA_AWT_LIBRARY)
-      SET (JAVA_AWT_LIBRARY "-framework JavaVM" CACHE FILEPATH "Java Frameworks" FORCE)
-    ENDIF(NOT JAVA_AWT_LIBRARY)
-    IF(NOT JAVA_JVM_LIBRARY)
-      SET (JAVA_JVM_LIBRARY "-framework JavaVM" CACHE FILEPATH "Java Frameworks" FORCE)
-    ENDIF(NOT JAVA_JVM_LIBRARY)
-    SET(JAVA_AWT_INCLUDE_DIRECTORIES ${JAVA_AWT_INCLUDE_DIRECTORIES}
-      ~/Library/Frameworks/JavaVM.framework/Headers
-      /Library/Frameworks/JavaVM.framework/Headers
-      /System/Library/Frameworks/JavaVM.framework/Headers
-      )
-  ENDIF(JAVA_HAVE_FRAMEWORK)
-ELSE(APPLE)
-  FIND_LIBRARY(JAVA_AWT_LIBRARY jawt 
-    PATHS ${JAVA_AWT_LIBRARY_DIRECTORIES}
-  )
-  FIND_LIBRARY(JAVA_JVM_LIBRARY NAMES jvm JavaVM
-    PATHS ${JAVA_JVM_LIBRARY_DIRECTORIES}
-  )
-ENDIF(APPLE)
+    FIND_LIBRARY(JAVA_AWT_LIBRARY jawt
+        PATHS ${JAVA_AWT_LIBRARY_DIRECTORIES}
+    )
+    FIND_LIBRARY(JAVA_JVM_LIBRARY NAMES jvm JavaVM
+        PATHS ${JAVA_JVM_LIBRARY_DIRECTORIES}
+    )
 
-# add in the include path    
-FIND_PATH(JAVA_INCLUDE_PATH jni.h 
-  ${JAVA_AWT_INCLUDE_DIRECTORIES}
-)
+    # add in the include path
+    FIND_PATH(JAVA_INCLUDE_PATH jni.h
+        ${JAVA_AWT_INCLUDE_DIRECTORIES}
+    )
+    FIND_PATH(JAVA_INCLUDE_PATH2 jni_md.h
+        ${JAVA_AWT_INCLUDE_DIRECTORIES}
+        ${JAVA_INCLUDE_PATH}/win32
+        ${JAVA_INCLUDE_PATH}/linux
+    )
 
-FIND_PATH(JAVA_INCLUDE_PATH2 jni_md.h 
-  ${JAVA_AWT_INCLUDE_DIRECTORIES}
-  ${JAVA_INCLUDE_PATH}/win32
-  ${JAVA_INCLUDE_PATH}/linux
-)
+    FIND_PATH(JAVA_AWT_INCLUDE_PATH jawt.h
+        ${JAVA_AWT_INCLUDE_DIRECTORIES} ${JAVA_INCLUDE_PATH}
+    )
 
-FIND_PATH(JAVA_AWT_INCLUDE_PATH jawt.h 
-          ${JAVA_AWT_INCLUDE_DIRECTORIES} ${JAVA_INCLUDE_PATH} )
+    MARK_AS_ADVANCED(
+        JAVA_AWT_LIBRARY
+        JAVA_JVM_LIBRARY
+        JAVA_AWT_INCLUDE_PATH
+        JAVA_INCLUDE_PATH
+        JAVA_INCLUDE_PATH2
+    )
 
-MARK_AS_ADVANCED(
-  JAVA_AWT_LIBRARY
-  JAVA_JVM_LIBRARY
-  JAVA_AWT_INCLUDE_PATH
-  JAVA_INCLUDE_PATH
-  JAVA_INCLUDE_PATH2
-)
+endif(JAVA_INCLUDE_PATH AND JAVA_JVM_LIBRARY)
